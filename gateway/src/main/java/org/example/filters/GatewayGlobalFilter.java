53772c8e114bbca456c82bceb2e9a1defc954e1d
GuoRui <G.163.com>
2025-05-12 13:37:27 +0800

    gateway Update

--- gateway/src/main/java/org/example/filters/GatewayGlobalFilter.java
+++ gateway/src/main/java/org/example/filters/GatewayGlobalFilter.java
@@ -2,11 +2,15 @@ package org.example.filters;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -32,22 +36,32 @@ public class GatewayGlobalFilter implements GlobalFilter, Ordered {
         ServerHttpRequest request = exchange.getRequest();
         String path = request.getPath().value();
         String method = request.getMethod().toString();
-        String sourceAddress = Objects.requireNonNull(request.getLocalAddress()).getHostString();
 
+        log.info("请求唯一标识：{}", request.getId());
+        log.info("请求路径：{}", path);
+        log.info("请求方法：{}", method);
+        log.info("请求参数：{}", request.getQueryParams());
         // 获取网关本地地址
-        String localAddress = request.getLocalAddress().getHostString();
+        String localAddress = Objects.requireNonNull(request.getLocalAddress()).getHostString();
         log.info("网关接收地址: {}", localAddress);
-
         // 获取客户端地址（可能为代理IP）
         String remoteAddress = Objects.requireNonNull(request.getRemoteAddress()).getHostString();
         log.info("直接连接地址: {}", remoteAddress);
-
         // 获取真实客户端IP（需配置代理支持 -- 反向代理时处理好才可以）
         String realClientIp = request.getHeaders().getFirst("X-Forwarded-For");
         log.info("真实客户端IP: {}", realClientIp);
 
-        System.out.println("请求URI: " + exchange.getRequest().getURI());
-        return chain.filter(exchange);
+        ServerHttpResponse response = exchange.getResponse();
+        if(!IP_WHITE_LIST.contains(remoteAddress)){ // todo 代理 / 真实 ??
+            response.setStatusCode(HttpStatus.FORBIDDEN);
+            return response.setComplete();
+        }
+        return handleResponse(exchange, chain, 1, 1);
+//        return chain.filter(exchange);
+    }
+
+    public Mono<Void> handleResponse(ServerWebExchange exchange, GatewayFilterChain chain, long interfaceInfoId, long userId) {
+
     }
 
     @Override
